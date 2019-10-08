f3059dbf09f48418e1ea6301ae7fe70613df5007
Dr. John Zoidberg <27244574+MrJohnZoidberg@users.noreply.github.com>
2019-10-08 21:30:58 +0200

    Initial commit

---
+++ action-domi-Bluetooth.py
@@ -0,0 +1,80 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import bluetooth  # install bluez-development
+import paho.mqtt.client as mqtt
+import json
+import toml
+
+
+USERNAME_INTENTS = "domi"
+MQTT_BROKER_ADDRESS = "localhost:1883"
+MQTT_USERNAME = None
+MQTT_PASSWORD = None
+
+
+def add_prefix(intent_name):
+    return USERNAME_INTENTS + ":" + intent_name
+
+
+def get_slots(data):
+    slot_dict = {}
+    try:
+        for slot in data['slots']:
+            if slot['value']['kind'] in ["InstantTime", "TimeInterval", "Duration"]:
+                slot_dict[slot['slotName']] = slot['value']
+            elif slot['value']['kind'] == "Custom":
+                slot_dict[slot['slotName']] = slot['value']['value']
+    except (KeyError, TypeError, ValueError) as e:
+        print("Error: ", e)
+        slot_dict = {}
+    return slot_dict
+
+
+def on_message_scan(client, userdata, msg):
+    data = json.loads(msg.payload.decode("utf-8"))
+    session_id = data['sessionId']
+    intent_id = data['intent']['intentName']
+
+    say(session_id, "Bluetooth Suche wurde gestartet.")
+
+    nearby_devices = bluetooth.discover_devices(lookup_names=True)
+    print("found %d devices" % len(nearby_devices))
+
+    for addr, name in nearby_devices:
+        print("  %s - %s" % (addr, name))
+
+
+def say(session_id, text):
+    mqtt_client.publish('hermes/dialogueManager/endSession', json.dumps({'text': text,
+                                                                         'sessionId': session_id}))
+
+
+def end_session(session_id):
+    mqtt_client.publish('hermes/dialogueManager/endSession', json.dumps({'sessionId': session_id}))
+
+
+def dialogue(session_id, text, intent_filter, custom_data=None):
+    data = {'text': text,
+            'sessionId': session_id,
+            'intentFilter': intent_filter}
+    if custom_data:
+        data['customData'] = json.dumps(custom_data)
+    mqtt_client.publish('hermes/dialogueManager/continueSession', json.dumps(data))
+
+
+if __name__ == "__main__":
+    snips_config = toml.load('/etc/snips.toml')
+    if 'mqtt' in snips_config['snips-common'].keys():
+        MQTT_BROKER_ADDRESS = snips_config['snips-common']['mqtt']
+    if 'mqtt_username' in snips_config['snips-common'].keys():
+        MQTT_USERNAME = snips_config['snips-common']['mqtt_username']
+    if 'mqtt_password' in snips_config['snips-common'].keys():
+        MQTT_PASSWORD = snips_config['snips-common']['mqtt_password']
+
+    mqtt_client = mqtt.Client()
+    mqtt_client.message_callback_add('hermes/intent/' + add_prefix('BluetoothDevicesScan'), on_message_scan)
+    mqtt_client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+    mqtt_client.connect(MQTT_BROKER_ADDRESS.split(":")[0], int(MQTT_BROKER_ADDRESS.split(":")[1]))
+    mqtt_client.subscribe('hermes/intent/' + add_prefix('BluetoothDevicesScan'))
+    mqtt_client.loop_forever()
