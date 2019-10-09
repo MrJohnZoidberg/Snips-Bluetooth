14c66dfa664fe8c2dafe046e07a67d145237a79b
Dr. John Zoidberg <27244574+MrJohnZoidberg@users.noreply.github.com>
2019-10-09 19:08:07 +0200

    Update

--- action-domi-Bluetooth.py
+++ action-domi-Bluetooth.py
@@ -6,6 +6,7 @@ import paho.mqtt.client as mqtt
 import json
 import toml
 import threading
+import configparser
 
 
 USERNAME_INTENTS = "domi"
@@ -18,16 +19,29 @@ def add_prefix(intent_name):
     return USERNAME_INTENTS + ":" + intent_name
 
 
+def read_configuration_file(configuration_file):
+    try:
+        cp = configparser.ConfigParser()
+        with open(configuration_file, encoding="utf-8") as f:
+            cp.read_file(f)
+        return {section: {option_name: option for option_name, option in cp.items(section)}
+                for section in cp.sections()}
+    except (IOError, configparser.Error):
+        return dict()
+
+
 class Bluetooth:
     def __init__(self):
         self.nearby_devices = list()
         self.scan_thread = None
+        synonym_list = config['global']['device_synonyms'].split(',')
+        self.synonyms = {synonym.split('::')[0]: synonym.split('::')[1] for synonym in synonym_list}
 
     def scan_devices(self):
         self.nearby_devices = bluetooth.discover_devices(lookup_names=True)
         device_names = [name for addr, name in self.nearby_devices]
         if device_names:
-            inject('bluetooth_devices', ["Kombo 22", "Fernseher", "Internetradio"])
+            inject('bluetooth_devices', device_names, "add_devices")
         else:
             notify("Ich habe kein Gerät gefunden.")
 
@@ -64,7 +78,10 @@ def on_message_devices_say(client, userdata, msg):
 
     part = ""
     for addr, name in devices:
-        part += name
+        if name in bluetooth_cls.synonyms:
+            part += bluetooth_cls.synonyms[name]
+        else:
+            part += name
         if name != devices[-1][1]:
             part += ", "
 
@@ -77,12 +94,15 @@ def on_message_devices_say(client, userdata, msg):
 
 
 def on_message_injection_complete(client, userdata, msg):
-    if bluetooth_cls.scan_thread:
-        del bluetooth_cls.scan_thread
-    if len(bluetooth_cls.nearby_devices) > 1:
-        notify("Ich habe {num} Geräte gefunden.".format(num=len(bluetooth_cls.nearby_devices)))
-    else:
-        notify("Ich habe ein Gerät gefunden.")
+    data = json.loads(msg.payload.decode("utf-8"))
+
+    if data['requestId'] == "add_devices":
+        if bluetooth_cls.scan_thread:
+            del bluetooth_cls.scan_thread
+        if len(bluetooth_cls.nearby_devices) > 1:
+            notify("Ich habe {num} Geräte gefunden.".format(num=len(bluetooth_cls.nearby_devices)))
+        else:
+            notify("Ich habe ein Gerät gefunden.")
 
 
 def say(session_id, text):
@@ -99,10 +119,10 @@ def notify(text):
                                                                                     'text': text}}))
 
 
-def inject(entity_name, values, operation_kind='addFromVanilla'):
+def inject(entity_name, values, request_id, operation_kind='addFromVanilla'):
     operation_data = {entity_name: values}
     operation = (operation_kind, operation_data)
-    mqtt_client.publish('hermes/injection/perform', json.dumps({'operations': [operation]}))
+    mqtt_client.publish('hermes/injection/perform', json.dumps({'id': request_id, 'operations': [operation]}))
 
 
 def dialogue(session_id, text, intent_filter, custom_data=None):
@@ -122,6 +142,9 @@ if __name__ == "__main__":
         MQTT_USERNAME = snips_config['snips-common']['mqtt_username']
     if 'mqtt_password' in snips_config['snips-common'].keys():
         MQTT_PASSWORD = snips_config['snips-common']['mqtt_password']
+
+    config = read_configuration_file('config.ini')
+    default_config = read_configuration_file('config.ini.default')
 
     bluetooth_cls = Bluetooth()
     mqtt_client = mqtt.Client()
