17a7a9d7dc989ec23b32992dd347c14f11b6e5d9
Dr. John Zoidberg <27244574+MrJohnZoidberg@users.noreply.github.com>
2019-10-11 17:12:17 +0200

    Update

--- bluetoothctl.py
+++ bluetoothctl.py
@@ -174,7 +174,7 @@ class Bluetoothctl:
             logger.error(e)
             return False
         else:
-            res = self.process.expect(
-                "Connected: no"
+            res = self.process.expect_list(
+                ["Failed to disconnect", "Connected: no", pexpect.EOF]
             )
             return res == 1
